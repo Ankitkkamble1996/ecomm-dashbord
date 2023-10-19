fe1bb16469a664b458fff24ce3691e9838edb481
Ankit Kamble <ankitrkamble@gmail.com>
2023-10-19 12:29:36 +0530

    Ankit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap'
+import Header from './Header'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from './Login'
+import Register from './Register'
+import AddProduct from './AddProduct'
+import UpdateProduct from './UpdateProduct'
+
 
 
 function App() {
   return (
     <div className="App">
+     
+     <BrowserRouter>
+      <Header></Header>
       <h1>E-Commerce Project</h1>
-      <button>Normal Button</button>
-      <Button>Bootstrap Button</Button>
+     
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/add" element={<AddProduct />} />
+        <Route path="/update" element={<UpdateProduct />} />
+      </Routes>
+    
+     
+     </BrowserRouter>
+     
     </div>
   );
 }
